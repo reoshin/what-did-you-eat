777ab8d04d4164d0efcaff7a7f4814caaffc6d04
Reo Shin <jungyeop@students.cs.ubc.ca>
2026-02-12 15:20:59 -0800

    Added Specification for FoodList

---
+++ src/main/model/FoodList.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.ArrayList;
+
+// Represents a list of food added by user
+public class FoodList {
+    private ArrayList<Food> foodList;
+    
+    // EFFECTS: construct foodList with no food inside
+    public FoodList() {
+        foodList = new ArrayList<Food>();
+    }
+
+    // EFFECTS: add food in the list. If there is food with the same name already exist, 
+    //          delete previous one and replace by new food
+    // MODIFIES: this
+    public void addFood() {
+        // stub
+    }
+
+    // EFFECTS: find such food with the given foodName, and eats one time and returns calories consumed.
+    // MODIFIES: Food
+    public int eatFood() { // stub
+        return 0;
+    }
+}
