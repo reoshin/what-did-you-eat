aa1c1a03cbdf069f17c30c256055cbeed40d4745
Reo Shin <jungyeop@students.cs.ubc.ca>
2026-02-13 13:43:56 -0800

    Added specification for new method for FoodList Class

--- src/main/model/FoodList.java
+++ src/main/model/FoodList.java
@@ -14,13 +14,18 @@ public class FoodList {
     // EFFECTS: add food in the list. If there is food with the same name already exist, 
     //          delete previous one and replace by new food
     // MODIFIES: this
-    public void addFood(Food food) { // stub
-        
+    public void addFood(Food food) { 
+        // stub
     }
 
     // EFFECTS: find such food with the given foodName, and eats one time and returns calories consumed.
     // MODIFIES: Food
     public int eatFood() { // stub
         return 0;
+    }
+
+    // EFFECTS: find such food with given name and returns true if food is found
+    public Boolean findFood(String foodName) { // stub
+        return false;
     }
 }
