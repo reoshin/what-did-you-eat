32cde2502bcce5b15cf3d7f01b418fc3dfeba82e
Reo Shin <jungyeop@students.cs.ubc.ca>
2026-02-13 13:29:40 -0800

    Added specification for Week

---
+++ src/main/model/Week.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+
+// represents a week
+public class Week {
+    private ArrayList<Day> week;
+    
+    // EFFECTS: initialize week with 7 days
+    public Week() {
+        // stub
+    }
+}
