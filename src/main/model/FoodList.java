090d52224f4e91493422a954363c84e858aa27de
Reo Shin <jungyeop@students.cs.ubc.ca>
2026-02-13 23:14:22 -0800

    Implemented FoodList

--- src/main/model/FoodList.java
+++ src/main/model/FoodList.java
@@ -11,21 +11,38 @@ public class FoodList {
         foodList = new ArrayList<Food>();
     }
 
-    // EFFECTS: add food in the list. If there is food with the same name already exist, 
-    //          delete previous one and replace by new food
+    // EFFECTS: add food in the list if food is not in a list already
     // MODIFIES: this
     public void addFood(Food food) { 
-        // stub
+        if(! foodList.contains(food)) {
+            foodList.add(food);
+        }
     }
 
     // EFFECTS: find such food with the given foodName, and eats one time and returns calories consumed.
+    // REQUIRES: there should be food with given name in FoodList
     // MODIFIES: Food
-    public int eatFood() { // stub
-        return 0;
+    public int eatFood(String foodName) { // stub
+        Food currentFood = findFood(foodName);
+        currentFood.eatFood();
+        return currentFood.getCalories();
     }
 
-    // EFFECTS: find such food with given name and returns true if food is found
-    public Boolean findFood(String foodName) { // stub
-        return false;
+    // EFFECTS: find such food with given name, otherwise return null
+    public Food findFood(String foodName) { // stub
+        for(Food currentFood : foodList) {
+            if(foodName.equals(currentFood.getName())) {
+                return currentFood;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Food> getFoodList() {
+        return foodList;
+    }
+
+    public Food getNthFood(int index) {
+        return foodList.get(index);
     }
 }
