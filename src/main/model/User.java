3abd6b66b9b3da76f2afe25e2487e4d471515d23
Reo Shin <jungyeop@students.cs.ubc.ca>
2026-02-13 23:14:46 -0800

    Implemented User and changed some specification

--- src/main/model/User.java
+++ src/main/model/User.java
@@ -1,12 +1,15 @@
 package model;
 
+import java.util.ArrayList;
+
 // represent User with daily calories goal (in kcal), daily calories consumed (in kcal),
 // success status and daily score (out of 100)
 public class User {
     private int dailyCaloriesGoal;
-    private double dailyScore;
+    private int dailyScore;
     private int dailyCaloriesConsumed;
-    private static double INITIAL_DAILYSCORE = 50.0;
+    private static int INITIAL_DAILYSCORE = 50;
+    private FoodList foodList;
 
     // REQUIRES: dailyCaloriesGoal >= 0
     // EFFECTS: Constructs user with given dailyCaloriesGoal, initial dailyScore,
@@ -15,22 +18,53 @@ public class User {
         this.dailyCaloriesGoal = dailyCaloriesGoal;
         this.dailyScore = INITIAL_DAILYSCORE;
         this.dailyCaloriesConsumed = 0;
+        foodList = new FoodList();
     }
 
+    public int getDailyCaloriesGoal() {
+        return dailyCaloriesGoal;
+    }
+
+    public int getDailyScore() {
+        return dailyScore;
+    }
+
+    public int getDailyCaloriesConsumed() {
+        return dailyCaloriesConsumed;
+    }
+
+    public FoodList getFoodList() {
+        return foodList;
+    }
+
+    public void addFood(String foodName, int calories) {
+        foodList.addFood(new Food(foodName, calories));
+    }
+
+
+    // REQUIRES: there should be a food with given name
     // EFFECTS: find food with foodName from FoodList and record as it has eaten
     //          and add the calories of that food into daily, update daily score and return true.
     //          If can't find such food, return false and do nothing.
-    public Boolean eatFood(String foodName) {
-        return false;
+    public void recordFood(String foodName) {
+        this.dailyCaloriesConsumed += foodList.eatFood(foodName);
+        updateDailyScore();
     }
     
     // REQUIRES: dailyCaloriesConsumed > 0
     // MODIFIES: this
-    // EFFECTS: set the score as INITIAL_SCORE + (50 * (dailyCaloriesConsumed / dailyCaloriesGoal))
+    // EFFECTS: set the score as INITIAL_DAILYSCORE + 50 * (dailyCaloriesConsumed / dailyCaloriesGoal)
     //          if dailyCaloriesConsumed <= dailyCaloriesGoal,
-    //          otherwise, set the score as INITIAL_SCORE - (50 * (dailyCaloriesGoal / dailyCaloriesConsumed))
+    //          otherwise, set the score as INITIAL_SCORE - 50 * (dailyCaloriesConsumed - dailyCaloriesGoal) / dailyCaloriesGoal)
     public void updateDailyScore() {
-        // stub
+        if (dailyCaloriesConsumed <= dailyCaloriesGoal) {
+            double score = INITIAL_DAILYSCORE + 50.0 * ((double) dailyCaloriesConsumed / dailyCaloriesGoal);
+            dailyScore = (int) Math.round(score);
+        } else {
+            double score = INITIAL_DAILYSCORE - 50.0 * 
+                            ((double) (dailyCaloriesConsumed - dailyCaloriesGoal) / dailyCaloriesGoal);
+            dailyScore = (int) Math.round(score);
+        }
     }
 
     // EFFECTS: return user's sucess status by determining following criteria
@@ -39,6 +73,13 @@ public class User {
     //          return ":/" if 25 < dailyScore < 50
     //          otherwise return ":("
     public String getSuccessStatus() {
-        return "";
+        if(dailyScore >= 75) {
+            return ":D";
+        } else if(dailyScore >= 50) {
+            return ":)";
+        } else if(dailyScore > 25) {
+            return ":/";
+        }
+        return ":(";
     }
 }
