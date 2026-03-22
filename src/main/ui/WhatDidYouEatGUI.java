a2373c4cfa8e67532f6268a8842498d6f9b80401
Reo Shin <jungyeop@students.cs.ubc.ca>
2026-03-21 20:55:01 -0700

    Provided implementation for initialize GUI method and persistence UI

--- src/main/ui/WhatDidYouEatGUI.java
+++ src/main/ui/WhatDidYouEatGUI.java
@@ -2,14 +2,18 @@ package ui;
 
 import java.util.Scanner;
 
-import javax.swing.JComboBox;
-import javax.swing.JDesktopPane;
-import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
-
+import model.FoodList;
 import model.User;
 import persistence.JsonReader;
 import persistence.JsonWriter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 // represents application's main window.
 public class WhatDidYouEatGUI extends JFrame{
@@ -21,21 +25,71 @@ public class WhatDidYouEatGUI extends JFrame{
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
 
+    private JButton addButton;
+    private JButton printButton;
+    private JButton logButton;
+    private JButton saveButton;
+    private JButton loadButton;
+
     // Constructor sets up visual window and user.
     public WhatDidYouEatGUI() {
-        // stub
+        super("What Did You Eat?");
+        initializeFields();
+        initializeGraphics();
+        initializeInteraction();
     }
 
     // EFFECTS: initialize json writer and reader.
     // MODIFIES: this
     public void initializeFields() {
-        // stub
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
     }
 
     // MODIFIES: this
     // EFFECTS: initialize JFrame window where WhatDidYouEat will operate, and populates the tools to be used
     public void initializeGraphics() {
-        // stub
+        setTitle("What Did You Eat?");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);
+
+        askForLoadFoodlist();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: asks user to load foodlist or not.
+    private void askForLoadFoodlist() {
+        Object[] options = {"Yes", "No"};
+
+        int choice = JOptionPane.showOptionDialog(null, 
+            "Would you like to load the data?", 
+            "Load Data", 
+            JOptionPane.YES_NO_CANCEL_OPTION, 
+            JOptionPane.QUESTION_MESSAGE,
+             null, 
+             options, 
+             options[0]);
+        if (choice == JOptionPane.YES_OPTION) {
+            loadFoodList();
+        } else {
+            JOptionPane.showMessageDialog(null, "Starting without loading data.");
+        }
+
+        setVisible(true);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads workroom from file
+    private void loadFoodList() {
+        try {
+            FoodList fl = jsonReader.read();
+            user.loadFoodList(fl);
+            JOptionPane.showMessageDialog(null, "Loaded your food list from " + JSON_STORE);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Unable to read from file: " + JSON_STORE);
+        }
     }
 
     // MODIFIES: this
@@ -44,10 +98,10 @@ public class WhatDidYouEatGUI extends JFrame{
         // stub
     }
 
-    // MODIFIES: thjis
+    // MODIFIES: this
     // EFFECTS: a helper method which declares and instaniates all options.
     private void createMenu() {
-        // stub
+        
     }
 }
 
