8104b2456b3edf38fc76847aa188686962846066
Reo Shin <jungyeop@students.cs.ubc.ca>
2026-03-26 14:24:10 -0700

    Added tests for Event and EventLog

---
+++ src/test/model/EventTest.java
@@ -0,0 +1,33 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.assertEquals;
+
+import model.Event;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class EventTest {
+    private Event e;
+    private Date d;
+
+    @BeforeEach
+    public void runBefore() {
+        e = new Event("Event occured");
+        d = Calendar.getInstance().getTime();
+    }
+
+    @Test
+    public void testEvent() {
+        assertEquals("Event occured", e.getDescription());
+        assertEquals(d, e.getDate());
+    }
+
+    @Test
+    public void textToString() {
+        assertEquals(d.toString() + "\n" + "Event occured", e.toString());
+    }
+}
